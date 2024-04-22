2aec3a5b7db345633bc0ae99ee7e251fe8d0f3b0
Djibsone <djibriltamou@gmail.com>
2024-04-22 12:09:55 +0100

    first commit

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $cridentiels = $request->only('email', 'password');
+
+        $token = Auth::attempt($cridentiels);
+
+        if (!$token) {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Non authorisé.',
+                ],
+                401,
+            );
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ],
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = Auth::login($user);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Registration avec succès.',
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ],
+        ]);
+    }
+
+    public function userDetails()
+    {
+        return response()->json(auth()->user());
+    }
+}
